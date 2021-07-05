4e3b1bc08742f65a7ce3a5472c171eac2345af0a
0rGaan1c <nathan4drake1999@gmail.com>
2021-07-05 09:53:07 +0530

    faq accordion - mobile desing

--- faq-accordion-card/src/App.js
+++ faq-accordion-card/src/App.js
@@ -13,19 +13,33 @@ const App = () => {
       <div className="app">
         <div className="app__img">
           <div className="app__img__woman"></div>
-          <div className="app_img_pattern"></div>
+          <div className="app__img__pattern"></div>
         </div>
         <h1>FAQ</h1>
-        {faqs.map((faq, idx) => {
-          const { id, question, answer } = faq;
+        <div className="app__faq__container">
+          {faqs.map((faq, idx) => {
+            const { id, question, answer } = faq;
 
-          return (
-            <div className="app__faq" key={idx}>
-              <h3> {question}</h3>
-              <h3>{answer}</h3>
-            </div>
-          );
-        })}
+            return (
+              <div className="app__faq" key={idx}>
+                <div className="app__faq__question">
+                  <h3>{question}</h3>
+                  <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M1 .799l4 4 4-4"
+                      stroke="#F47B56"
+                      stroke-width="2"
+                      fill="none"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <h3 className="app__faq__answer">{answer}</h3>
+                <div className="app__faq__border"></div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
