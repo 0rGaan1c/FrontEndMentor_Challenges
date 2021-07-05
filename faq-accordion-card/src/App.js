0018d370eb874cef17d1db8f9464218be26ad569
0rGaan1c <nathan4drake1999@gmail.com>
2021-07-05 09:27:24 +0530

    faq according-initial mobile design

--- faq-accordion-card/src/App.js
+++ faq-accordion-card/src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./App.css";
+import { faqs } from "./faqs";
+
+// <div class="attribution">
+//   Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+//   Coded by <a href="#">Your Name Here</a>.
+// </div>
 
 const App = () => {
-  return <div>Something is not right</div>;
+  return (
+    <>
+      <div className="app">
+        <div className="app__img">
+          <div className="app__img__woman"></div>
+          <div className="app_img_pattern"></div>
+        </div>
+        <h1>FAQ</h1>
+        {faqs.map((faq, idx) => {
+          const { id, question, answer } = faq;
+
+          return (
+            <div className="app__faq" key={idx}>
+              <h3> {question}</h3>
+              <h3>{answer}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default App;
